0ac5a4621244808bff0dd5ab1b63309315e2257f
Antwon Jones <antwonjones02@users.noreply.github.com>
2025-03-13 19:02:51 -0400

    Add Layout component

---
+++ components/Layout.tsx
@@ -0,0 +1,29 @@
+import React, { ReactNode, useState } from 'react';
+import Sidebar from './Sidebar';
+import Navbar from './Navbar';
+
+interface LayoutProps {
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  return (
+    <div className="flex h-screen bg-neutral-50 dark:bg-neutral-900">
+      {/* Sidebar */}
+      <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
+
+      {/* Main Content */}
+      <div className="flex flex-col flex-1 overflow-hidden">
+        <Navbar onMenuButtonClick={() => setSidebarOpen(true)} />
+        
+        <main className="flex-1 overflow-y-auto p-4 md:p-6">
+          {children}
+        </main>
+      </div>
+    </div>
+  );
+};
+
+export default Layout;
