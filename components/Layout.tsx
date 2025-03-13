5bf908bb6330136801b11bf1722f3303e2928838
Antwon Jones <antwonjones02@users.noreply.github.com>
2025-03-13 19:28:11 -0400

    Fix sidebar overlap issue

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -11,11 +11,11 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
 
   return (
     <div className="flex h-screen bg-neutral-50 dark:bg-neutral-900">
-      {/* Sidebar */}
+      {/* Sidebar - Hidden on mobile, visible on desktop */}
       <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
 
       {/* Main Content */}
-      <div className="flex flex-col flex-1 overflow-hidden">
+      <div className="flex flex-col flex-1 md:ml-64 overflow-hidden">
         <Navbar onMenuButtonClick={() => setSidebarOpen(true)} />
         
         <main className="flex-1 overflow-y-auto p-4 md:p-6">
