ed0e93f0250ac5ef092dfba48b38b2f45d975826
Antwon Jones <antwonjones02@users.noreply.github.com>
2025-03-13 19:44:35 -0400

    Update Layout component with less bright background colors

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -10,14 +10,14 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
   const [sidebarOpen, setSidebarOpen] = useState(false);
 
   return (
-    <div className="min-h-screen bg-gray-100 dark:bg-gray-900">
+    <div className="min-h-screen bg-neutral-100 dark:bg-[#041C2C]">
       <Sidebar isOpen={sidebarOpen} setIsOpen={setSidebarOpen} />
       
       <div className="md:pl-64 flex flex-col flex-1">
         <Navbar setSidebarOpen={setSidebarOpen} />
         
         <main className="flex-1">
-          <div className="py-6">
+          <div className="py-6 px-4 sm:px-6 md:px-8">
             {children}
           </div>
         </main>
